b9e81536939e5a3a0a5a17d94132b19b157b362e
jkeppeler <jkeppeler@parthenonsoftware.com>
2021-05-14 17:19:11 -0700

    remove old debug

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -144,8 +144,8 @@ require("../css/toastr.min.css");
 
                 dashboard.engine.on('status', function(status) {
                     // console.log('Status Object');
-                    console.log("status state:  "+status.state);
-                    console.log(status);
+                    // console.log("status state:  "+status.state);
+                    // console.log(status);
                     if(status.state == 'dead') {
                         dashboard.showModal({
                             title: 'An Error Occurred!',
